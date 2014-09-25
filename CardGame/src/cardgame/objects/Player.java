2b5dded4bb5274755b68c0bad189290288265273
02104 <02104@000a140imcwl032.district87.org>
2014-09-25 15:12:56 -0500

    Update

--- CardGame/src/cardgame/objects/Player.java
+++ CardGame/src/cardgame/objects/Player.java
@@ -2,22 +2,16 @@ package cardgame.objects;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
 
 public class Player extends GameObject {
-	Random r = new Random();
+	
 	public Player(int x, int y, ObjectIDs id) {
 		super(x, y, id);
 	}
 
 	public void tick() {
-		/*if (r.nextInt(2) == 1) {
-			x += r.nextInt(10);
-			y += r.nextInt(10);
-		} else {
-			x -= r.nextInt(10);
-			y -= r.nextInt(10);
-		}*/
+		x += getVelX();
+		y += getVelY();
 	}
 
 	public void render(Graphics g) {
--- CardGame/src/cardgames/games/Space_Invaders.java
+++ CardGame/src/cardgames/games/Space_Invaders.java
@@ -1,5 +1,8 @@
 package cardgames.games;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import cardgame.objects.Alien;
 import cardgame.objects.ObjectIDs;
 import cardgame.objects.Player;
@@ -7,33 +10,62 @@ import cardgames.main.CardGame;
 import cardgames.main.ObjectHandler;
 
 
-public class Space_Invaders {
+public class Space_Invaders implements KeyListener {
 	ObjectHandler oHandler = CardGame.getObjectHandler();
-	Alien[] alien;
+	//List<Alien> alien = new ArrayList<Alien>();
+	//Alien[] alien;
+	Player p1 = new Player(200, 300, ObjectIDs.Player);
+	int X_BOUND = CardGame.WIDTH;
+	int Y_BOUND = CardGame.HEIGHT;
+	
 	public Space_Invaders() {
-		int SPACING = 64;
-		Alien a1 = new Alien(0, 50, ObjectIDs.Alien);
-		Alien a2 = new Alien((SPACING += SPACING), 50, ObjectIDs.Alien);
-		Alien a3 = new Alien((SPACING += SPACING), 50, ObjectIDs.Alien);
-		Alien a4 = new Alien((SPACING += SPACING), 50, ObjectIDs.Alien);
-		Alien a5 = new Alien((SPACING += SPACING), 50, ObjectIDs.Alien);
-		a1.setVelX(2);
-		a2.setVelX(2);
-		a3.setVelX(2);
-		a4.setVelX(2);
-		a5.setVelX(2);
-		oHandler.addObject(a1);
-		oHandler.addObject(a2);
-		oHandler.addObject(a3);
-		oHandler.addObject(a4);
-		oHandler.addObject(a5);
-		oHandler.addObject(new Player(1280 / 2 - 16, 720 - 100, ObjectIDs.Player));
+		oHandler.addObject(p1);
 	}
 	
-	private void addAliens(int quantity) {
-		for (int i = 0; i < quantity; i++) {
-			alien[0] = {oHandler.addObject(new Alien(0, 50, ObjectIDs.Alien))};
-			
+	private void addAlien(int quantity) {
+		
+	}
+	
+	private Alien getAlien(int i) {
+		return alien[i];
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyCode() == 77) {
+			p1.setVelY(3);
 		}
 	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
+/*public Space_Invaders() {
+int SPACING = 64;
+Alien a1 = new Alien(0, 50, ObjectIDs.Alien);
+Alien a2 = new Alien((SPACING += SPACING), 50, ObjectIDs.Alien);
+Alien a3 = new Alien((SPACING += SPACING), 50, ObjectIDs.Alien);
+Alien a4 = new Alien((SPACING += SPACING), 50, ObjectIDs.Alien);
+Alien a5 = new Alien((SPACING += SPACING), 50, ObjectIDs.Alien);
+a1.setVelX(2);
+a2.setVelX(2);
+a3.setVelX(2);
+a4.setVelX(2);
+a5.setVelX(2);
+oHandler.addObject(a1);
+oHandler.addObject(a2);
+oHandler.addObject(a3);
+oHandler.addObject(a4);
+oHandler.addObject(a5);
+oHandler.addObject(new Player(1280 / 2 - 16, 720 - 100, ObjectIDs.Player));
+}*/
